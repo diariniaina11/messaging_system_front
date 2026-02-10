0b9bb0c7d26d3b460f5f812d3ddf645b2ff3ce73
diariniaina11 <jm.diariniaina@gmail.com>
2026-02-10 11:59:12 +0300

    Setting up sidebar skeleton

---
+++ src/components/sidebar/sideBar.js
@@ -0,0 +1,35 @@
+import styles from "./sideBar.module.css"
+export default function SideBar()
+{
+    return (
+        <div className={styles.sidebarContainer}>
+            <ul className={styles.topElement}>
+                <li>
+                    <img src="business_messages_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
+                </li>
+                <li>
+                    <img src="circle_circle_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
+                </li>
+                <li>
+                    <img src="bigtop_updates_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
+                </li>
+                <li>
+                    <img src="groups_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
+                </li>
+                
+            </ul>
+            <ul className={styles.bottomElement}>
+                <li>
+                    <img src="art_track_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
+                </li>
+                <li>
+                    <img src="settings_b_roll_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
+                </li>
+                <li>
+                    <img src="account_box_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
+                </li>
+                
+            </ul>
+        </div>   
+    )
+}
