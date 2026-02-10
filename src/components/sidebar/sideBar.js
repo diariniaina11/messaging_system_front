baeda6990d130c6638007e204cf0fb75df04508f
diariniaina11 <jm.diariniaina@gmail.com>
2026-02-10 12:24:50 +0300

    adding hover feature for each items of navbar

--- src/components/sidebar/sideBar.js
+++ src/components/sidebar/sideBar.js
@@ -4,28 +4,29 @@ export default function SideBar()
     return (
         <div className={styles.sidebarContainer}>
             <ul className={styles.topElement}>
-                <li>
+                <li className={`${styles.li}`}>
+                    <li></li>
                     <img src="business_messages_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
                 </li>
-                <li>
+                <li className={`${styles.li}`}>
                     <img src="circle_circle_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
                 </li>
-                <li>
+                <li className={`${styles.li}`}>
                     <img src="bigtop_updates_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
                 </li>
-                <li>
+                <li className={`${styles.li}`}>
                     <img src="groups_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
                 </li>
                 
             </ul>
             <ul className={styles.bottomElement}>
-                <li>
+                <li className={`${styles.li}`}>
                     <img src="art_track_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
                 </li>
-                <li>
+                <li className={`${styles.li}`}>
                     <img src="settings_b_roll_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
                 </li>
-                <li>
+                <li className={`${styles.li}`}>
                     <img src="account_box_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></img>
                 </li>
                 
