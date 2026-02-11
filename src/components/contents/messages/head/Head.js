c1933499bebe528f6179a867ed22f2e69b1daedb
diariniaina11 <jm.diariniaina@gmail.com>
2026-02-11 20:30:24 +0300

    apparence : done

--- src/components/contents/messages/head/Head.js
+++ src/components/contents/messages/head/Head.js
@@ -1,21 +1,21 @@
 
 import { useState } from "react";
 import styles from "./Head.module.css";
-export default function Head(){
-    
-    const [inputIsFocused, setInputIsFocused]=useState(false);
-    const [buttonSelection, setButtonSelection]=useState({
-        all:true,
-        nSeen:false,
-        favorite:false,
-        group:false
+export default function Head() {
+
+    const [inputIsFocused, setInputIsFocused] = useState(false);
+    const [buttonSelection, setButtonSelection] = useState({
+        all: true,
+        nSeen: false,
+        favorite: false,
+        group: false
     });
 
 
-    function handleInputFocusStyle(state){
+    function handleInputFocusStyle(state) {
         setInputIsFocused(state)
     }
-    function handleSelectedFilter(){}
+    function handleSelectedFilter() { }
 
     return (
         <div className={styles.container}>
@@ -23,38 +23,50 @@ export default function Head(){
                 <h3 className={styles.appName}>
                     WhatsApp
                 </h3>
-                
+
                 <ul className={styles.ul}>
                     <li>
-                        <img src="add_comment_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg"/>
+                        <img src="add_comment_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" />
                     </li>
                     <li>
-                        <img src="more_vert_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg"/>
-                        
-                    </li>  
-                </ul>  
+                        <img src="more_vert_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" />
+
+                    </li>
+                </ul>
             </div>
             <div className={styles.filter}>
-                <div 
-                    className={styles.searchInput} 
-                    style={(inputIsFocused)? {border:'2px solid #20c063', background:'none'}:{border:'2px solid #2e2f2f'} }
+                <div
+                    className={styles.searchInput}
+                    style={(inputIsFocused) ? { border: '2px solid #20c063', background: 'none' } : { border: '2px solid #2e2f2f' }}
                 >
-                    <img src="search_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg"/>
-                    
-                    <input 
-                        onFocus={()=>{handleInputFocusStyle(true)}} 
-                        onBlur={()=>{handleInputFocusStyle(false)}} 
-                        style={(inputIsFocused)? {background:'none'}:{} }/>
+                    <img src="search_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" />
+
+                    <input
+                        onFocus={() => { handleInputFocusStyle(true) }}
+                        onBlur={() => { handleInputFocusStyle(false) }}
+                        style={(inputIsFocused) ? { background: 'none' } : {}} />
                 </div>
-                
+
                 <div className={styles.filterButtonContainer}>
-                    <button className={styles.filterButton}><b>Toutes</b></button>
-                    <button className={styles.filterButton}><b>Non lues</b></button>
-                    <button className={styles.filterButton}><b>Favoris</b></button>
-                    <button className={styles.filterButton}><b>Groupes</b></button>
+                    <button
+                        className={`${styles.filterButton} ${buttonSelection.all ? styles.activeFilter : ''}`}
+                        onClick={() => setButtonSelection({ all: true, nSeen: false, favorite: false, group: false })}
+                    ><b>Toutes</b></button>
+                    <button
+                        className={`${styles.filterButton} ${buttonSelection.nSeen ? styles.activeFilter : ''}`}
+                        onClick={() => setButtonSelection({ all: false, nSeen: true, favorite: false, group: false })}
+                    ><b>Non lues</b></button>
+                    <button
+                        className={`${styles.filterButton} ${buttonSelection.favorite ? styles.activeFilter : ''}`}
+                        onClick={() => setButtonSelection({ all: false, nSeen: false, favorite: true, group: false })}
+                    ><b>Favoris</b></button>
+                    <button
+                        className={`${styles.filterButton} ${buttonSelection.group ? styles.activeFilter : ''}`}
+                        onClick={() => setButtonSelection({ all: false, nSeen: false, favorite: false, group: true })}
+                    ><b>Groupes</b></button>
                 </div>
             </div>
         </div>
-        
+
     );
 }
